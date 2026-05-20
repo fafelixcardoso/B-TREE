23eae19503e03cc12a26d91c9b318f40d819abc9
fafelixcardoso <fabriciofcjunior@gmail.com>
2026-05-20 17:10:52 -0300

    Código B+ Tree

--- btree.c
+++ btree.c
@@ -19,7 +19,7 @@ typedef struct auxNo {
   // Chaves:
   // Nos internos: servem apenas como índices de roteamento.
   // Nas folhas: são as chaves dos registros reais.
-  TipoChave chaves[2*T-1];
+  TipoChave chaves[2*T-1]; //fica fora da union, todos tem chaves (ou pra rotear ou guardar)
 
   union {
       // Campos EXCLUSIVOS de Nós Internos
@@ -54,8 +54,13 @@ void exibeNo(No* atual){
   int x;
   printf("Chaves no No atual [%p]: %i\n", atual, atual->numChaves);
   for(x=0;x<atual->numChaves;x++){
-    printf("%i ", atual->regs[x].chave);
-  }
+
+    if (atual->folha) { //se o nó for folha, ele entra na parte das folhas info.folha, entra nos registros e busca a chavae
+    printf("%i ", atual->info.folha.regs[x].chave);
+    } else { // se for interno exibe a chave (sinalização)
+        printf("%i ", atual->chaves[x]);
+        }
+    }
   printf("\n");
 }
 
@@ -63,7 +68,8 @@ int altura(No* atual){
   if (atual->folha) return 0;
   int x, temp, max = 0;  
   for(x=0;x<=atual->numChaves;x++){
-    temp = altura(atual->filhos[x]);
+
+    temp = altura(atual->info.interno.filhos[x]); //o caminho pra acessar o filho mudou, antes era atual->filhos[x]
     if (temp>max) max = temp;
   }
   return max+1;
@@ -71,10 +77,14 @@ int altura(No* atual){
 
 
 void exibeEnderecos(No* atual){
+    //antes o código assumia q todo mundo tinha filho
+  if (atual->folha) return;  
   int x;
+
   printf("Links no No atual [%p]: %i\n", atual, atual->numChaves+1);
   for(x=0;x<atual->numChaves+1;x++){
-    printf("%p ", atual->filhos[x]);
+    //o caminho do filho mudou, igual a da funçao de cima
+    printf("%p ", atual->info.interno.filhos[x]);
   }
   printf("\n");
 }
@@ -82,9 +92,11 @@ void exibeEnderecos(No* atual){
 void exibeArvore(No* atual, int nivel){
   printf("Nivel: %i - ", nivel);
   exibeNo(atual);
-  if (atual->folha) return;
-  int i;
-  for (i=0;i<=atual->numChaves;i++) exibeArvore(atual->filhos[i], nivel+1);
+
+  if (atual->folha) return; //a condicao de folha já existia antes
+
+  int i; //mesmo caminho do filho
+  for (i=0;i<=atual->numChaves;i++) exibeArvore(atual->info.interno.filhos[i], nivel+1);
 }
 
 
@@ -97,17 +109,27 @@ void lerDoDisco(No* no){
 }
 
 
+
 bool buscaRegistroAux(No* atual, TipoChave ch, Registro* reg){
-  int i = 0;
-  while (i < atual->numChaves && ch > atual->regs[i].chave) i++;
-  if (i < atual->numChaves && ch == atual->regs[i].chave){
-    *reg = atual->regs[i];
-    return true;
-  }else{
-    if(!atual->folha){
-      return buscaRegistroAux(atual->filhos[i], ch, reg);
+    int i = 0;
+    //se for folha a chave tá no registro
+    if (atual->folha) {
+      while (i < atual->numChaves && ch > atual->info.folha.regs[i].chave) i++;
+	
+    } else {//se for interno a chave tá no array
+      while (i < atual->numChaves && ch > atual->chaves[i]) i++;
     }
-  }
+
+    //se ele for folha, achou a chave, copia e retorna true
+    if (atual->folha && i < atual->numChaves && ch == atual->info.folha.regs[i].chave){
+      *reg = atual->info.folha.regs[i];
+      return true;
+
+    }else{ //se não achou e for interno, busca no próximo
+      if(!atual->folha){
+        return buscaRegistroAux(atual->info.interno.filhos[i], ch, reg);
+      }
+  }// se não achou em nenhum lugar retorna falso
   return false;
 }
 
@@ -116,20 +138,43 @@ bool buscaRegistro(ArvB* a, TipoChave ch, Registro* reg){
 }
 
 
-
 void divideNoFilho(No* x, int i, No* y){
   No* novo = (No*) malloc(sizeof(No));
   novo->folha = y->folha;
-  novo->numChaves = T - 1;
   int j;
-  for (j=0; j<T-1; j++) novo->regs[j] = y->regs[j+T];
+
+  if (y->folha) {
+      //o novo nó fica com T elementos
+      novo->numChaves = T;
+      for (j=0; j<T; j++) {
+        // move um pra trás pra pegar o meio
+        novo->info.folha.regs[j] = y->info.folha.regs[j+T-1];
+      }
+      //liga o ponteiro da lista quando a folha racha
+      novo->info.folha.prox = y->info.folha.prox;
+      y->info.folha.prox = novo;
+
+  } else {
+      //se for interno o tam fica igual da B e a chave sobe  
+      novo->numChaves = T - 1;
+      for (j=0; j<T-1; j++) novo->chaves[j] = y->chaves[j+T];
+  }
+
+
   if (!y->folha)
-    for (j=0; j<T; j++) novo->filhos[j] = y->filhos[j+T];
-  y->numChaves = T - 1;
-  for (j=x->numChaves; j>i; j--) x->filhos[j+1] = x->filhos[j];
-  x->filhos[i+1] = novo;
-  for (j=x->numChaves-1; j>=i; j--) x->regs[j+1] = x->regs[j];
-  x->regs[i] = y->regs[T-1];
+    for (j=0; j<T; j++) novo->info.interno.filhos[j] = y->info.interno.filhos[j+T];
+  y->numChaves = T - 1; // o nó da esquerda fica com T-1
+
+  // o pai é interno, então ele não mexe nos registros
+  for (j=x->numChaves; j>i; j--) x->info.interno.filhos[j+1] = x->info.interno.filhos[j];
+  x->info.interno.filhos[i+1] = novo;
+  for (j=x->numChaves-1; j>=i; j--) x->chaves[j+1] = x->chaves[j]; //abriu espaco na placa (guia)
+
+  if (y->folha) {
+    x->chaves[i] = y->info.folha.regs[T-1].chave; // se rachou folha, copia do registro
+  } else {
+    x->chaves[i] = y->chaves[T-1]; //se rachou nó interno, pega a chave na array fora do union
+  }
   x->numChaves++;
   salvarNoDisco(y);
   salvarNoDisco(novo);
@@ -138,23 +183,30 @@ void divideNoFilho(No* x, int i, No* y){
 
 void insereNoNaoCheio(No* atual, Registro* reg){
   int i = atual->numChaves-1;
+
   if (atual->folha){
-    while (i>=0 && reg->chave < atual->regs[i].chave){
-      atual->regs[i+1] = atual->regs[i];
+            //mudou o caminho do registro
+    while (i>=0 && reg->chave < atual->info.folha.regs[i].chave){
+      atual->info.folha.regs[i+1] = atual->info.folha.regs[i];
       i--;
-    }
-    atual->regs[i+1] = *reg;
+    }   // mudou o caminho do registro
+    atual->info.folha.regs[i+1] = *reg;
     atual->numChaves++;
-    salvarNoDisco(atual);
+    salvarNoDisco(atual); 
   }else{
-    while (i>=0 && reg->chave < atual->regs[i].chave) i--;
+        // mudou o caminho da chave no nó interno
+    while (i>=0 && reg->chave < atual->chaves[i]) i--;
     i++;
-    lerDoDisco(atual->filhos[i]);
-    if(atual->filhos[i]->numChaves==2*T-1){
-      divideNoFilho(atual, i, atual->filhos[i]);
-      if (reg->chave > atual->regs[i].chave) i++;
-    }
-    insereNoNaoCheio(atual->filhos[i], reg);
+    // mudou o caminho do filho
+    lerDoDisco(atual->info.interno.filhos[i]);
+    //mudou o caminho do filho
+    if(atual->info.interno.filhos[i]->numChaves==2*T-1){
+        // mudou o caminho do filho
+      divideNoFilho(atual, i, atual->info.interno.filhos[i]);
+      // mudou o caminho da chave no nó interno
+      if (reg->chave > atual->chaves[i]) i++;
+    } //mudou o caminho da chave no nó intern
+    insereNoNaoCheio(atual->info.interno.filhos[i], reg);
   }
 }
 
@@ -167,13 +219,33 @@ void insere(ArvB* a, Registro* reg){
     No* r = a->raiz;
     a->raiz = novo;
     novo->numChaves = 0;
-    novo->folha = false;
-    novo->filhos[0] = r;
+    novo->folha = false; // a nova raíz nunca é folha pq ela vai ter filho
+
+    //muda o caminho do filho
+    novo->info.interno.filhos[0] = r; // a raiz antiga vira o primeiro filho da raiz nova
+
     divideNoFilho(novo, 0, r);
     insereNoNaoCheio(novo, reg);
   }
 }
 
+
+void imprimirFolhasSequencial(ArvB* a) {
+  No* atual = a->raiz;
+  if (atual->numChaves == 0) return;
+
+  //vai sempre pegar o primeiro filho, garantindo que vá ao filho mais a esquerda
+  while (!atual->folha) atual = atual->info.interno.filhos[0];
+
+  //percorrer a lista das folhas até o final
+  while (atual != NULL) {
+    for (int i = 0; i < atual->numChaves; i++) {
+      printf("%i ", atual->info.folha.regs[i].chave);
+    }
+    atual = atual->info.folha.prox;
+  }
+  printf("\n");
+}
 
 int main(){
 
